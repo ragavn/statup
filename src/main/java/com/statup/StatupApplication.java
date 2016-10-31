a236a58496e338dea9b9faae897ed3ba88909cb9
Ragav Nagarajan <ragav.n@outlook.com>
2016-10-31 14:58:12 +0800

    Initial application commit

---
+++ src/main/java/com/statup/StatupApplication.java
@@ -0,0 +1,12 @@
+package com.statup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatupApplication.class, args);
+	}
+}
