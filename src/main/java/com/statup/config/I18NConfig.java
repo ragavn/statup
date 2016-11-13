272035d4c14473e2487dfc4e61021c0155c6c66b
Ragav Nagarajan <ragav.n@outlook.com>
2016-11-13 11:02:41 +0800

    Added I18N support

---
+++ src/main/java/com/statup/config/I18NConfig.java
@@ -0,0 +1,17 @@
+package com.statup.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18NConfig {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setCacheSeconds(1800);
+		return messageSource;
+	}
+}
--- src/test/java/com/statup/StatupApplicationTests.java
+++ src/test/java/com/statup/StatupApplicationTests.java
@@ -1,16 +1,27 @@
 package com.statup;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.statup.web.i18n.I18NService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StatupApplicationTests {
+	
+	@Autowired
+	public I18NService i18NService;
 
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocalService() throws Exception {
+		String expectedResult = "Bootstrap starter template";
+		String messageId = "index.main.callout";
+		String actual = i18NService.getMessage(messageId);
+		Assert.assertEquals("Does not match", expectedResult, actual);
 	}
 
 }
